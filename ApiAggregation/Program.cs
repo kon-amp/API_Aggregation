a22d286e3e49a37c879e9d560669cedbe8e13423
kon-amp <kon.ampatzidis@gmail.com>
2024-06-28 20:45:57 +0300

    refactor(program): simplify the code as all services have been moved to startup

--- ApiAggregation/Program.cs
+++ ApiAggregation/Program.cs
@@ -2,28 +2,13 @@ using ApiAggregation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Add the Startup class configuration
-Startup? startup = new Startup(builder.Configuration);
+// Add services to the container with Startup class.
+var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
-WebApplication? app = builder.Build();
 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline with Startup class.
 startup.Configure(app, app.Environment);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
 app.Run();
