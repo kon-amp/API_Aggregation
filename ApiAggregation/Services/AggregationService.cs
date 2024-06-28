869c3bae2fc3325db28d93dd7dc332248c5f2a64
kon-amp <kon.ampatzidis@gmail.com>
2024-06-28 20:48:53 +0300

    refactor(service): update AggregationService for new GitHub integration

--- ApiAggregation/Services/AggregationService.cs
+++ ApiAggregation/Services/AggregationService.cs
@@ -1,4 +1,5 @@
 ﻿using ApiAggregation.Models;
+using ApiAggregation.Models.Github;
 using ApiAggregation.Models.News;
 using ApiAggregation.Models.Spotify;
 using ApiAggregation.Models.Weather;
@@ -10,27 +11,31 @@ namespace ApiAggregation.Services
         private readonly WeatherService _weatherService;
         private readonly NewsService _newsService;
         private readonly SpotifyService _spotifyService;
+        private readonly GithubService _githubService;
 
-        public AggregationService(WeatherService weatherService, NewsService newsService, SpotifyService spotifyService)
+        public AggregationService(WeatherService weatherService, NewsService newsService, SpotifyService spotifyService, GithubService githubService)
         {
             _weatherService = weatherService;
             _newsService = newsService;
             _spotifyService = spotifyService;
+            _githubService = githubService;
         }
 
-        public async Task<AggregatedData> GetAggregatedDataAsync(WeatherRequest weatherRequest, NewsRequest newsRequest, SpotifyArtistsRequest spotifyRequest)
+        public async Task<AggregatedData> GetAggregatedDataAsync(WeatherRequest weatherRequest, NewsRequest newsRequest, SpotifyArtistsRequest spotifyRequest, GithubRequest githubRequest)
         {
             Task<WeatherResponse> weatherTask = _weatherService.GetDataAsync(weatherRequest);
             Task<NewsResponse> newsTask = _newsService.GetDataAsync(newsRequest);
             Task<SpotifyResponse> spotifyTask = _spotifyService.GetDataAsync(spotifyRequest);
+            Task<GithubResponse> githubTask = _githubService.GetDataAsync(githubRequest);
 
-            await Task.WhenAll(weatherTask, newsTask, spotifyTask);
+            await Task.WhenAll(weatherTask, newsTask, spotifyTask, githubTask);
 
             return new AggregatedData
             {
                 Weather = await weatherTask,
                 News = await newsTask,
-                Spotify = await spotifyTask
+                Spotify = await spotifyTask,
+                Github = await githubTask
             };
         }
         
