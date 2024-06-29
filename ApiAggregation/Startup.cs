55597a2a3657d6d90e451d8dbb93d51b53d54b28
kon-amp <kon.ampatzidis@gmail.com>
2024-06-29 23:50:53 +0300

    refactor(Startup): utitlize new services, redis instantiation, swagger enum definitions as also add xml documentation comments to swagger configuration

--- ApiAggregation/Startup.cs
+++ ApiAggregation/Startup.cs
@@ -1,10 +1,17 @@
-﻿using ApiAggregation.Models.Github;
+﻿using ApiAggregation.Models.AppSettings;
+using ApiAggregation.Models.CountriesInfo;
+using ApiAggregation.Models.Github;
 using ApiAggregation.Models.News;
 using ApiAggregation.Models.Spotify;
 using ApiAggregation.Models.Weather;
 using ApiAggregation.Services;
+using ApiAggregation.Services.Integrations;
 using ApiAggregation.Services.Interfaces;
 using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ApiAggregation
 {
@@ -19,20 +26,34 @@ namespace ApiAggregation
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // Bind configuration settings
+            services.Configure<ApiSettings>(Configuration.GetSection("ApiSettings"));
+
+            // Configuration for Redis
+            services.AddSingleton<IConnectionMultiplexer>(
+                    ConnectionMultiplexer.Connect("localhost,abortConnect=false")
+            );
+            services.AddScoped<ICacheService, RedisCacheService>();
+
             // Add services to the container.
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+            });
 
             // Configure HttpClient for each service
             services.AddHttpClient<WeatherService>();
             services.AddHttpClient<NewsService>();
             services.AddHttpClient<SpotifyService>();
             services.AddHttpClient<GithubService>();
+            services.AddHttpClient<CountriesService>();
 
             // Register services with scoped lifetime
             services.AddScoped<IApiService<SpotifyArtistsRequest, SpotifyResponse>, SpotifyService>();
             services.AddScoped<IApiService<WeatherRequest, WeatherResponse>, WeatherService>();
             services.AddScoped<IApiService<NewsRequest, NewsResponse>, NewsService>();
             services.AddScoped<IApiService<GithubRequest, GithubResponse>, GithubService>();
+            services.AddScoped<IApiService<CountriesInfoRequest, CountriesInfoResponse>, CountriesService>();
             services.AddScoped<AggregationService>();
 
             // Add Swagger 
@@ -41,6 +62,11 @@ namespace ApiAggregation
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Fun Page", Version = "v1" });
                 c.EnableAnnotations();
+                c.UseInlineDefinitionsForEnums();
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
