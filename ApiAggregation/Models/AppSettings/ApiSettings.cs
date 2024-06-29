705f233a3ed749bc7a90fc2bb67eecb30a521a9c
kon-amp <kon.ampatzidis@gmail.com>
2024-06-29 23:46:51 +0300

    feat: use appsettings.json for some basic info of all integration api calls as also the class model for it to have access through application runtime

---
+++ ApiAggregation/Models/AppSettings/ApiSettings.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+namespace ApiAggregation.Models.AppSettings
+{
+    public class ApiSettings
+    {
+        public WeatherApiSettings Weather { get; set; } = new WeatherApiSettings();
+        public NewsApiSettings NewsApi { get; set; } = new NewsApiSettings();
+        public CountriesApiSettings CountriesApi { get; set; } = new CountriesApiSettings();
+        public SpotifyApiSettings SpotifyApi { get; set; } = new SpotifyApiSettings();
+        public GitHubApiSettings GitHubApi { get; set; } = new GitHubApiSettings();
+
+    }
+
+    public class GitHubApiSettings
+    {
+        public string BaseUrl { get; set; } = string.Empty;
+        public string AcceptHeader { get; set; } = string.Empty;
+        public string AuthorizationHeader { get; set; } = string.Empty;
+        public string? ApiVersion { get; set; }
+        public string DefaultType { get; set; } = "owner";
+        public string DefaultSort { get; set; } = "full_name";
+        public string DefaultDirection { get; set; } = "asc";
+        public int DefaultPerPage { get; set; } = 30;
+    }
+
+    public class NewsApiSettings
+    {
+        public string BaseUrl { get; set; } = string.Empty;
+        public string ApiKey { get; set; } = string.Empty;
+        public string DefaultCountry { get; set; } = "gr";
+    }
+
+    public class SpotifyApiSettings
+    {
+        public string BaseUrl { get; set; } = string.Empty;
+        public string AuthorizationHeader { get; set; } = string.Empty;
+    }
+
+    public class WeatherApiSettings
+    {
+        public string BaseUrl { get; set; } = string.Empty;
+        public string ApiKey { get; set; } = string.Empty;
+    }
+
+    public class CountriesApiSettings
+    {
+        public string BaseUrl { get; set; } = string.Empty;
+        public string FieldsForFiltering { get; set; } = string.Empty;
+    }
+
+}
