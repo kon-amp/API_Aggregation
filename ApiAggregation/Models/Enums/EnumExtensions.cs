d73a4d9c62ef404b2732028e3d5d5a8ae3443ea8
kon-amp <kon.ampatzidis@gmail.com>
2024-06-29 23:26:20 +0300

    feat: implement enum functionality to get string value of an enumeration

---
+++ ApiAggregation/Models/Enums/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace ApiAggregation.Models.Enums
+{
+    public static class EnumExtensions
+    {
+        public static string GetEnumMemberValue<T>(this T enumValue) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException("T must be an enumerated type");
+
+            // Retrieve the member info of the enum
+            var memberInfo = typeof(T).GetMember(enumValue.ToString()).FirstOrDefault();
+
+            // Get the EnumMemberAttribute data
+            var enumMemberAttribute = memberInfo?.GetCustomAttribute<EnumMemberAttribute>();
+
+            // Return the value specified in the EnumMember attribute
+            return enumMemberAttribute?.Value;
+        }
+    }
+}
