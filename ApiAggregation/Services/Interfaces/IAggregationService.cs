476314d7c37d96e86706f1542579b4b59f048092
kon-amp <kon.ampatzidis@gmail.com>
2024-06-30 18:25:16 +0300

    feat(interface): Add IAggregationService interface to decouple aggregation logic
    Introduced IAggregationService interface to abstract the aggregation service implementations.
    This change promotes better modularity and makes the AggregationService easier to manage and test.
    It allows dependency injection to utilize interface-based programming, enhancing the maintainability
    and scalability of the application's service layer.

---
+++ ApiAggregation/Services/Interfaces/IAggregationService.cs
@@ -0,0 +1,12 @@
+﻿using ApiAggregation.Models.CountriesInfo;
+using ApiAggregation.Models.News;
+using ApiAggregation.Models.Weather;
+using ApiAggregation.Models;
+
+namespace ApiAggregation.Services.Interfaces
+{
+    public interface IAggregationService
+    {
+        Task<AggregatedData> GetAggregatedDataAsync(WeatherRequest weatherRequest, NewsRequest newsRequest, CountriesInfoRequest countriesInfoRequest);
+    }
+}
