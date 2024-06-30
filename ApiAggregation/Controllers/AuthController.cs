d3057871b74a0ae34da17408f931ef38b0952cba
kon-amp <kon.ampatzidis@gmail.com>
2024-06-30 13:30:46 +0300

    feat: add implementation for jwt authentication

---
+++ ApiAggregation/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using ApiAggregation.Models.AppSettings;
+using ApiAggregation.Models.User;
+using ApiAggregation.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ApiAggregation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthTokenService _tokenService;
+        private readonly LoginModelSettings _settings;
+
+        public AuthController(AuthTokenService tokenService, IOptions<ApiSettings> settings)
+        {
+            _tokenService = tokenService;
+            _settings = _settings = settings.Value.LoginModel;
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public IActionResult Login([FromQuery] LoginModel login)
+        {
+            if (string.Equals(login.Username, _settings.Username) && string.Equals(login.Password, _settings.Password))
+            {
+                var token = _tokenService.GenerateToken(login.Username);
+                return Ok(new { token });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
---
+++ ApiAggregation/Services/AuthTokenService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiAggregation.Services
+{
+    public class AuthTokenService
+    {
+        public string GenerateToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("kon_amp_assessment_interview_2024"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "Issuer",
+                audience: "Audience",
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
